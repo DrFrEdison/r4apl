4eca3ad9d874683c220daf4cd534a81c35bb62aa
DrFrEdison <kurtz@mailbox.org>
2025-01-16 14:02:39 +0100

    add renv

---
+++ R/global_color.R
@@ -0,0 +1,8 @@
+# Set global configuration for R-Scripts ####
+# r4apl color pch font etc ####
+r4apl <- list()
+r4apl$col <- c("#007C1D", "#104B99", "#6E3AC1", "#099191", "#B32724", "#F27900", "#EDAE00" # APL Corporate Identity Colors
+               , RColorBrewer::brewer.pal(5, "Set1")
+               , RColorBrewer::brewer.pal(8, "Dark2"))
+r4apl$pch <- c(16,0,18,2,15,6,17,1,4)
+r4apl$font <- "Verdana"
